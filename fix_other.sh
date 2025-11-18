e90c0f96c0390e82a566323168112300cff79bb2
17381792@qq.com <17381792@qq.com>
2025-11-18 09:30:09 +0800

    alter chagne

--- fix_other.sh
+++ fix_other.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
-cd /data/arm-bio/qtwebkit-Arm-5.9.0/Source/WebCore/generated
+cd /data/arm-bio/qtwebkit-A40/Source/WebCore/generated
 bison -d -p cssyy CSSGrammar.y
 mv CSSGrammar.tab.c CSSGrammar.cpp
 mv CSSGrammar.tab.h CSSGrammar.hpp
 ln -sf CSSGrammar.hpp CSSGrammar.tab.h
 
-cd /data/arm-bio/qtwebkit-Arm-5.9.0/Source/WebCore/xml
+cd /data/arm-bio/qtwebkit-A40/Source/WebCore/xml
 bison -d -p xpathyy XPathGrammar.y
 
 
